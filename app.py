5e51a3f47823c0edf47e11296109de2f0c196075
Shaikh Borhan Uddin <shaikhborhanuddin@gmail.com>
2026-03-01 12:01:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ with col17:
 with col18:
     st.subheader(" ")
 
-    deductible = st.number_input("Deductible", min_value=0, max_value=5000, value=500)
+    deductible = st.number_input("Deductible", min_value=0, max_value=10000, value=500)
     deductible_category = get_deductible_category(deductible)
     st.text_input("Deductible Level (Auto-calculated)", value=str(deductible_category), disabled=True)
     copay = st.number_input("Copay", min_value=0, max_value=100, value=20)
@@ -209,6 +209,7 @@ if st.button("Predict Annual Premium", type="primary"):
 
 st.markdown("---")
 st.markdown("Developed by Shaikh Borhan Uddin")
+
 
 
 
