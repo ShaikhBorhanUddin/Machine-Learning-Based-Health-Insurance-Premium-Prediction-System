32d87f8a3d54b58edb239750f5fab1509175dadd
Shaikh Borhan Uddin <shaikhborhanuddin@gmail.com>
2026-03-02 22:44:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ with col2:
     st.subheader(" ")
 
     education = st.selectbox("Education Level", ['College', 'Doctorate', 'High School Dropout', 'High School', 'Masters', 'Bachelors'])
-    income = st.number_input("Yearly Income", 10000, 10000000, 12000)
+    income = st.number_input("Yearly Income", 10000, 10000000, 50000)
     employment_status = st.selectbox("Employment Status", ['Employed', 'Self-employed', 'Retired', 'Unemployed'])
     region = st.selectbox("Geographical Region", ['North', 'Central', 'West', 'East', 'South'])
     urban_rural = st.selectbox("Area", ['Suburban', 'Urban', 'Rural'])
@@ -209,6 +209,7 @@ if st.button("Predict Annual Premium", type="primary"):
 
 st.markdown("---")
 st.markdown("Developed by Shaikh Borhan Uddin")
+
 
 
 
